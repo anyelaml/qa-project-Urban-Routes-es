180b921bb9837feec20c513c1d78b3d613761f26
anyelaml <anyela.malaga@ucsp.edu.pe>
2024-08-21 20:43:54 -0500

    Add files via upload

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-ded30c9c-bbdb-4905-8b09-a280675822af.containerhub.tripleten-services.com/?lng=es'
+urban_routes_url = 'https://cnt-2c79f134-e9ff-4f14-95d6-23dc7aa84a6f.containerhub.tripleten-services.com/?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
